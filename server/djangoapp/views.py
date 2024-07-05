f1efd384ac71e8c6d6ebed90cd93d680fe014155
Gislain <gislainmba@yahoo.fr>
2024-07-04 21:56:30 -0400

    pylint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -71,9 +71,11 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username,
-                                        first_name=first_name, last_name=last_name,
-                                        password=password, email=email)
+        user = User.objects.create_user(
+                    username=username, first_name=first_name,
+                    last_name=last_name,
+                    password=password, email=email
+                )
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName" : username, "status" : "Authenticated"}
@@ -105,7 +107,8 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel" : car_model.name, "CarMake" : car_model.car_make.name})
+        cars.append({"CarModel" : car_model.name,
+                    "CarMake" : car_model.car_make.name})
     return JsonResponse({"CarModels" : cars})
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
