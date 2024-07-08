94dd21991301b5ae6a26ab44456f8b6fd6665324
Gislain <gislainmba@yahoo.fr>
2024-07-08 07:15:39 -0400

    pylint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -148,7 +148,6 @@ def add_review(request):
     if (request.user.is_anonymous is False):
         # data = json.loads(request.body)
         try:
-        # response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse(
