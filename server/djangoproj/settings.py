0d7bbf0583dd674d8b614db58af56e9558dd5025
Gislain <gislainmba@yahoo.fr>
2024-07-08 10:27:35 -0400

    final changes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,12 +30,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://gislainmba-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01' +
+    'https://gislainmba-8000' +
+    '.theiadockernext-0-labs-prod-theiak8s-4-tor01' +
     '.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://https://gislainmba-8000' +
-    '.theiadockernext-1-labs-prod-theiak8s-4-tor01' +
+    'https://gislainmba-8000' +
+    '.theiadockernext-0-labs-prod-theiak8s-4-tor01' +
     '.proxy.cognitiveclass.ai'
 ]
 
